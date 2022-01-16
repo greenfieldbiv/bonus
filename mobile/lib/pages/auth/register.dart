79c35cdf5a65e088e4f9faf1630b7299ff8b270c
roman <rivanov@bivgrop.ru>
2022-01-16 20:48:31 +0300

    Mobile. Add storage configuration

--- mobile/lib/pages/auth/register.dart
+++ mobile/lib/pages/auth/register.dart
@@ -12,148 +12,116 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-                //    minHeight: viewportConstraints.maxHeight,
-
-                ),
+        child: Container(
+          // constraints: BoxConstraints.expand(),
+          child: Align(
+            alignment: Alignment.topCenter,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 150),
-                      child: Image.asset("images/logo.png"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30.0, right: 30, bottom: 30),
-                      child: Container(
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          minLines: 1,
-                          maxLines: 1,
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.mail_outline),
-                            hintText: 'Введите Email',
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30.0, right: 30, bottom: 30),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey[200],
-                              blurRadius: 6.0, // soften the shadow
-                              spreadRadius: 2.0, //extend the shadow
-                              offset: Offset(
-                                0, // Move to right 10  horizontally
-                                0, // Move to bottom 10 Vertically
-                              ),
-                            )
-                          ],
-                        ),
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          minLines: 1,
-                          maxLines: 1,
-                          autocorrect: false,
-                          decoration: InputDecoration(
-                            icon: Icon(Icons.mail_outline),
-                            hintStyle: GoogleFonts.cinzel(color: Colors.grey[500], fontSize: 16, fontWeight: FontWeight.normal),
-                            hintText: 'Пароль',
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(40.0)),
-                              borderSide: BorderSide(color: Colors.grey),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30.0, right: 30, bottom: 30),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 6.0, // soften the shadow
-                              spreadRadius: 2.0, //extend the shadow
-                              offset: Offset(
-                                0, // Move to right 10  horizontally
-                                0, // Move to bottom 10 Vertically
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 150),
+                  // child: Image.asset("images/logo.png"),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 28, left: 0.0),
-                      child: Container(
-                          height: 45,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            color: Colors.blueAccent[400],
-                            borderRadius: new BorderRadius.only(
-                              bottomLeft: const Radius.circular(5.0),
-                              bottomRight: const Radius.circular(5.0),
-                              topLeft: const Radius.circular(5.0),
-                              topRight: const Radius.circular(5.0),
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                              top: 0.0,
-                            ),
-                            child: Column(
-                              children: [
-                                InkWell(onTap: () {
-                                  var t = HttpCaller.post<dynamic>(new Uri.http('10.0.2.2:8080', 'auth/registry'),
-                                      params: json.encode({"email": _emailController.text, "password": _passwordController.text}),
-                                      headers: {"Content-Type": "application/json"}).then(
-                                    (value) => value != null
-                                        ? Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => LoginWidget(),
-                                            ))
-                                        : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                            content: Text("Не удалось авторизоваться"),
-                                          )),
-                                  );
-                                }),
-                              ],
-                            ),
-                          )),
+                SizedBox(
+                  height: 100,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0, right: 30, bottom: 30),
+                  child: Container(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      minLines: 1,
+                      maxLines: 1,
+                      autocorrect: false,
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.mail_outline),
+                        hintText: 'Email',
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
                     ),
-                  ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0, right: 30, bottom: 30),
+                  child: Container(
+                    child: TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      minLines: 1,
+                      maxLines: 1,
+                      autocorrect: false,
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.mail_outline),
+                        hintStyle: GoogleFonts.cinzel(color: Colors.grey[500], fontSize: 16, fontWeight: FontWeight.normal),
+                        hintText: 'Пароль',
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          HttpCaller.post<dynamic>(new Uri.http('10.0.2.2:8080', 'auth/registry'),
+                              params: json.encode({"email": _emailController.text, "password": _passwordController.text}),
+                              headers: {"Content-Type": "application/json"}).then(
+                            (value) => value != null
+                                ? Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => LoginWidget(),
+                                    ),
+                                  )
+                                : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                    content: Text("Не удалось авторизоваться"),
+                                  )),
+                          );
+                        },
+                        child: Text(
+                          'Зарегистрироваться',
+                          style: GoogleFonts.cinzel(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'Назад',
+                          style: GoogleFonts.cinzel(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
